bd0839b5f07dfa408e412ffd9201e54eeccdf1c3
ILIANASEMENOVA <129002657+ILIANASEMENOVA@users.noreply.github.com>
2023-12-05 22:28:46 +0200

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -20,6 +20,7 @@ const contactsSlice = createSlice({
 });
 
 export const { addContact, deleteContact } = contactsSlice.actions;
+
 export const contactsReducer = contactsSlice.reducer;
 
 export const getContacts = state => state.contacts.contacts;
